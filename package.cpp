0fec2b734d4c885a83d90f7a81b6dcbffd1791f3
Nickolay Ilyushin <nickolay02@inbox.ru>
2018-09-13 12:25:07 +0300

    Added particlepm manifest

---
+++ package.cpp
@@ -0,0 +1,17 @@
+#include <particlepm.hpp>
+
+using namespace PPM::Utils;
+
+PPM_PACKAGE(p) {
+  p->name("particledi");
+  p->version(PPM::Version("v0.3.1"));
+  p->description("A simple Dependency Injection helper");
+  p->github(PPM::GitHub("handicraftsman", "particledi"));
+  p->license(PPM::License("MIT", "https://github.com/handicraftsman/particledi/blob/master/LICENSE.txt"));
+
+  PPM::TargetPtr lib = p->library("particledi");
+  lib->cpp("c++11");
+  lib->cpp_files(std::vector<std::string> {
+    "particledi.cpp"
+  });
+}
